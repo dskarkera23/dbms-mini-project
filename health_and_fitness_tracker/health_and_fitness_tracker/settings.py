48d318d3d1711c14152c468c8ab91c3551a4ecc2
dskarkera23 <dskarkera23@gmail.com>
2023-11-10 18:14:42 +0530

    email_backend-done

--- health_and_fitness_tracker/health_and_fitness_tracker/settings.py
+++ health_and_fitness_tracker/health_and_fitness_tracker/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-tib*8)m-mg5nqn(-_gumsu%0+bt7y8&5y67f9l5g%=wy9d8fwn
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'health_and_fitness_tracker.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +107,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -120,7 +114,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
--- health_and_fitness_tracker/health_and_fitness_tracker/views.py
+++ health_and_fitness_tracker/health_and_fitness_tracker/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, HttpResponse
+from app.EmailBackEnd import EmailBackEnd
+from django.contrib.auth import authenticate, login, logout
 
 
 def BASE(request):
-    return render(request,'base.html')
+    return render(request, 'base.html')
 
 
 def LOGIN(request):
     return render(request, 'login.html')
+
+
+def doLogin(request):
+    if request.method == "POST":
+        user = EmailBackEnd.authenticate(request,
+                                         username=request.POST.get('email'),
+                                         password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            user_type = user.user_type
+            if user_type == '1':
+                return HttpResponse('This is User panel')
+            elif user_type == '2':
+                return HttpResponse('This is Trainer panel')
+            else:
+                # msg
+                return redirect('login')
+        else:
+            # msg
+            return redirect('login')
