86855d688eda5a4f141462bf14e4512b4ab9dc02
Darshan S Karkera <143337019+dskarkera23@users.noreply.github.com>
2024-06-16 17:52:20 +0530

    Update forms.py

--- health_and_fitness_tracker/app/forms.py
+++ health_and_fitness_tracker/app/forms.py
@@ -17,7 +17,7 @@ class FoodLogForm(forms.ModelForm):
         food_items = kwargs.pop('food_items', None)
         super(FoodLogForm, self).__init__(*args, **kwargs)
 
-        # Populate the 'food' field choices with the provided food_items queryset
+        
         if food_items:
             self.fields['food'].queryset = food_items
 
@@ -28,31 +28,21 @@ class BMILogForm(forms.ModelForm):
         fields = ['height', 'weight', 'log_date']
 
 
-# forms.py
-# app/forms.py
 
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
-        fields = ['content']  # Include the receiver field
+        fields = ['content'] 
 
 
-# app/forms.py
 
-# app/forms.py
-
-# app/forms.py
-
-# app/forms.py
-
-# Update TrainerMessageForm to filter clients for the logged-in trainer
 class TrainerMessageForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         trainer = kwargs.pop('trainer', None)
         super(TrainerMessageForm, self).__init__(*args, **kwargs)
 
         if trainer:
-            # Set the trainer as the sender and update the queryset for the receiver field
+   
             self.fields['sender'].initial = trainer
             self.fields['sender'].widget = forms.HiddenInput()
             self.fields['receiver'].queryset = CustomUser.objects.filter(selected_trainer=trainer, user_type='1')
